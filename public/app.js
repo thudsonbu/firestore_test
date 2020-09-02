8687f8e5af5aae372c1a76780e46b19da9cb8b7d
thudsonbu <hudson_thom@bentley.edu>
2020-09-02 19:27:34 -0400

    Firebase database querry

--- public/app.js
+++ public/app.js
@@ -1,8 +1,19 @@
 document.addEventListener("DOMContentLoaded", event => {
+    // staring firebase engine
     const app = firebase.app();
-    consolelog(app)
+    console.log(app);
+    // accessing firebase database
+    const db = firebase.firestore();
+    const myPost = db.collection('potts').doc('firstpost');
+    myPost.get()
+        .then(doc => {
+            const data = doc.data();
+            document.write( data.title + '<br>')
+        })
+
 })
 
+// user authentication
 function googleLogin() {
     const provider = new firebase.auth.GoogleAuthProvider(); 
     firebase.auth().signInWithPopup(provider)
